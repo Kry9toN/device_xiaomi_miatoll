74584f9c4dffadb15762e2971e495389d773589d
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-04 09:33:57 +0300

    sm6250-common: switch to aidl lights hal

--- common.mk
+++ common.mk
@@ -163,7 +163,7 @@ PRODUCT_COPY_FILES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sm6250
+    android.hardware.lights-service.xiaomi_sm6250
 
 # Media
 PRODUCT_COPY_FILES += \
